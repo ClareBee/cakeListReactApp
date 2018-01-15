8b8b5c283eaec5d0ae76abc2f4ca18ed83b2e018
Clare <chblackburne@gmail.com>
2018-01-15 17:33:01 +0000

    Adds initial fetch request to api to retrieve cake data

--- src/containers/CakesContainer.js
+++ src/containers/CakesContainer.js
@@ -7,6 +7,23 @@ class CakesContainer extends React.Component {
       cakes: []
     }
   }
+  componentDidMount(){
+    const cakeUrl = "http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes";
+    fetch(cakeUrl)
+    .then(function(response) {
+      if(response.ok){
+        return response.json();
+      }
+      throw new Error("Network response wasn't ok");
+    })
+    .then(data => {
+      this.setState({cakes: data});
+      console.log(data);
+    })
+    .catch(function(error){
+      console.log(error.message);
+    });
+  }
 
   render(){
     return(
