3feb116c120b7d9303599f924526a5455ca47995
Clare <chblackburne@gmail.com>
2018-01-15 20:56:49 +0000

    Links up cake object with Cake class

--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,25 +1,14 @@
 import React from 'react';
 
 class Cake extends React.Component {
-
-  componentWillUpdate(){
-    console.log("hello");
-    const chosenCakeUrl = "http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes/`${this.props.match.params.id}`";
-    fetch(chosenCakeUrl)
-    .then(function(response) {
-      if(response.ok){
-        return response.json();
-      }
-      throw new Error("Network response wasn't ok");
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(function(error){
-      console.log(error.message);
-    });
+  constructor(props){
+    super(props);
+    this.state = {
+      cake: {}
+    }
   }
-  render(){
+
+  componentDidMount(){
     const chosenCakeUrl = `http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes/${this.props.match.params.id}`;
     fetch(chosenCakeUrl)
     .then(function(response) {
@@ -29,22 +18,22 @@ class Cake extends React.Component {
       throw new Error("Network response wasn't ok");
     })
     .then(data => {
-      console.log(data);
+      this.setState({cake: data});
     })
     .catch(function(error){
       console.log(error.message);
     });
-    // if(!this.props.cake){
-    //   return null
-    // }
-    // var selectedCake = this.props.cake;
-    // console.log(selectedCake);
-    console.log(this.props);
-    console.log(this.props.match.params.id);
+  }
+
+  render(){
+    if(!this.state.cake){
+      return null
+    }
+
     return(
       <div>
         <p>This is the individual cake</p>
-
+        <p>{this.state.cake.name}</p>
       </div>
     );
   }
