ec38f0ec298fd90fcd2ba5fdb5a25f3e7b48af12
Clare <chblackburne@gmail.com>
2018-01-16 13:05:04 +0000

    Adds handleChange to form input to capture value of user input

--- src/components/CakeForm.js
+++ src/components/CakeForm.js
@@ -10,15 +10,44 @@ class CakeForm extends React.Component {
       comment: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addCake = this.addCake.bind(this);
   }
 
+  handleChange(e){
+    const nameInput = e.target.name;
+    const urlInput = e.target.imageUrl;
+    const commentInput = e.target.comment;
+    const ratingInput = e.target.yumFactor;
+    console.log(name.value);
+
+    this.setState({
+      name: nameInput,
+      imageUrl: urlInput,
+      yumFactor: ratingInput,
+      comment: commentInput
+    });
+  }
   handleSubmit(e){
     console.log("something submitted");
     e.preventDefault();
-    const newCake = new FormData(e.target);
+    const newCake = new FormData(e.target.value);
     //fetch post to /cakes url
-    
+    console.log(newCake);
+    // this.addCake(newCake)
   }
+
+  addCake(cake){
+    const url = "http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes";
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(cake),
+      headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+}
 
   render(){
     console.log(this.state.name);
@@ -26,10 +55,10 @@ class CakeForm extends React.Component {
       <React.Fragment>
         <h1>this will be the cake form</h1>
         <form action="/cakes" method="" onSubmit={this.handleSubmit}>
-          <input required type="text" value={this.state.name} name="name" placeholder="name of cake"/>
-          <input required type="url" name="imageUrl" placeholder="url for an image"/>
-          <input required type="number" min="0" max="5" name="yumFactor" placeholder="rating/5"/>
-          <input required type="text" name="comment" placeholder="comment"/>
+          <input onChange={this.handleChange} required type="text" name="name" placeholder="name of cake"/>
+          <input onChange={this.handleChange} required type="url" name="imageUrl" placeholder="url for an image"/>
+          <input onChange={this.handleChange} required type="number" min="1" max="5" name="yumFactor" placeholder="rating/5"/>
+          <input onChange={this.handleChange} required type="text" name="comment" placeholder="comment"/>
           <button type="submit">Submit</button>
         </form>
       </React.Fragment>
